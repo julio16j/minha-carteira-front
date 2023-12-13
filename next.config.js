18fb148261cfb30cd965c7db14f31f4290dd8855
Julio Cesar <jc.urbano.vasconcelos@gmail.com>
2023-12-13 14:19:32 -0300

    fix unused dependencies

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         BRAPI_TOKEN: process.env.BRAPI_TOKEN,
         BRAPI_BASE_URL: process.env.BRAPI_BASE_URL,
         MINHA_CARTEIRA_BASE_URL: process.env.MINHA_CARTEIRA_BASE_URL
-      }
+    }
 }
 
 module.exports = nextConfig
